93a2da6341f1731c109c3ac6505f1ce35b0d6084
Abhishek <abhirahul6@gmail.com>
2018-12-27 22:45:07 +0530

    added test script for /todo/id

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/todos/:id',(req,res)=>{
 	}
 	Todo.findById(id).then((todo)=>{
 		if(!todo){
-			return res.status(400).send('id not found');
+			return res.status(404).send('id not found');
 		}
 		res.send({todo});
 	}).catch((e)=>{
