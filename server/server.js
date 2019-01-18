cc21d2bd798d49ab7c6e6d78a00623836ee19756
Abhishek <abhirahul6@gmail.com>
2019-01-18 18:07:31 +0530

    added authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./Db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -102,6 +103,11 @@ app.post('/users',(req, res)=>{
 	}).catch((e)=>{
 		res.status(400).send(e);
 	})
+});
+
+
+app.get('/user/me', authenticate, (req,res)=>{
+	res.send(req.user);
 });
 
 
